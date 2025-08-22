ba82381ada74a1c70a359ecf4d3e960bdd1affa7
Imanh728 <imanhussain728@gmail.com>
2025-08-22 19:46:06 +0100

    Vector.java

--- Vector.java
+++ Vector.java
@@ -3,36 +3,35 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
-//Vector class implements cloneable and iterable
 public class Vector implements Cloneable, Iterable<Double>{
 
-    //Encapsulation stores coordnates in private list
+   
 private List<Double> components;
 
-//constructor
+
 public Vector(List<Double> values){
-    this.components = new ArrayList<>(values); //defensive copy
+    this.components = new ArrayList<>(values);
 }
 
-     //Public interface methods 
+     
 
-//get(i): returns coordinate at index i
+
 public double get(int i){
     return components.get(i);
 }
 
 
-//set(int i,val) update coordinate
+
 public void set(int i, double val){
     components.set(i,val);
 }
 
-//length() returns number of dimension
+
 public int length(){
     return components.size();
 }
 
-//add(other) return new vector that is the sum
+
 public Vector add(Vector other){
     if(this.length() != other.length()){
         throw new IllegalArgumentException("Dimension must match");
@@ -46,7 +45,7 @@ public Vector add(Vector other){
 
 }
 
-//subtract(other ) return new vector that is the difference
+
 public Vector subtract(Vector other){
     if(this.length() != other.length()){
         throw new IllegalArgumentException("Dimension must match");
@@ -60,19 +59,19 @@ public Vector subtract(Vector other){
 
 }
 
-//clone() deep copy
+
 @Override
 public Vector clone(){
     return new Vector(this.components);
 }
 
-//toString() string representation
+
 @Override
 public String toString(){
     return components.toString();
 }
 
-// equals(Object o) check equality
+
 @Override
 public boolean equals(Object o){
     if(this == o) return true;
@@ -81,7 +80,7 @@ public boolean equals(Object o){
     return Objects.equals(this.components, v.components);
 }
 
-//Iteration implement Iterable<Double>
+
 @Override
 public Iterator<Double> iterator(){
     return components.iterator();
